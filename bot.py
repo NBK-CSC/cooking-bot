46ca080720f83bb9d7b50e986a62fe801170a2b0
Yagoroff <igor.igor260902@yandex.ru>
2022-03-06 22:23:08 +0300

    add buttons from cuisines of the world

--- bot.py
+++ bot.py
@@ -1,11 +1,38 @@
 import telebot
-
+from telebot import types
 import config
 
 bot = telebot.TeleBot(config.TOKEN, parse_mode=None)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-bot.polling(none_stop=True)
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "{0.first_name}, вас приветствует бот-повар 👨‍🍳".format(message.from_user))
+
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.send_message(message, "")
+
+@bot.message_handler(func=lambda m: True)
+def country_kitchen(message):
+    if message.text == "кухни мира":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('Итальянская🇮🇹')
+        item2 = types.KeyboardButton('Русская🇷🇺')
+        item3 = types.KeyboardButton('Японская🇯🇵')
+        item4 = types.KeyboardButton('Французская🇫🇷')
+        item5 = types.KeyboardButton('Вьетнамская🇻🇳')
+        item6 = types.KeyboardButton('Китайская🇨🇳')
+        item7 = types.KeyboardButton('Грузинская🇬🇪')
+        item8 = types.KeyboardButton('Корейская🇰🇷')
+
+        markup.add(item1, item2, item3, item4, item5, item6, item7, item8)
+
+        bot.send_message(message.chat.id,
+                         "Если не нашли нужную вам страну, то напишите: Кухня %Название страны%".format(
+                             message.from_user), reply_markup=markup)
+    else:
+        bot.reply_to(message, "Извините, но я не понимаю о чем вы")
+
+bot.infinity_polling()
