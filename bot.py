36f114a688a370e6592ce1c5b3a41c7664182d68
Yagoroff <igor.igor260902@yandex.ru>
2022-03-07 12:23:36 +0300

    add buttons for /help and /start

--- bot.py
+++ bot.py
@@ -8,4 +8,4 @@ bot = telebot.TeleBot(config.TOKEN, parse_mode=None)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
-bot.polling(none_stop=True)
+bot.polling(none_stop=True)123
