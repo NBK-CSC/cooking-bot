53dc33c08f57955a674bcceeb3771486445ffe75
Yagoroff <igor.igor260902@yandex.ru>
2022-03-07 12:21:45 +0300

    add buttons for /help and /start

--- bot.py
+++ bot.py
@@ -7,16 +7,28 @@ bot = telebot.TeleBot(config.TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "{0.first_name}, вас приветствует бот-повар 👨‍🍳".format(message.from_user))
+    stic = open('stic/hello.webp', 'rb')
+    bot.send_message(message.chat.id, "{0.first_name}, вас приветствует бот Шеф-Повар 👨‍🍳".format(message.from_user))
+    bot.send_sticker(message.chat.id, stic)
+    bot.send_message(message.chat.id, "Для ознакомления с функционалом бота, пропишите команду /help")
 
 
 @bot.message_handler(commands=['help'])
 def handle_help(message):
-    bot.send_message(message, "")
+    markup_for_help = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Готовка блюд')
+    item2 = types.KeyboardButton('Подсчет калорий')
+
+    markup_for_help.add(item1, item2)
+
+    bot.send_message(message.chat.id, 'У бота есть две категории:\n\n\t<b>1.Готовка блюд</b> 🥘\n\t\t➡\tТут вы можете найти '
+                                      'рецепты для блюд и способы их приготовления\n\n\t<b>2.Подсчет калорий</b>\n\t\t➡\t',
+                     reply_markup=markup_for_help, parse_mode="html")
+
 
 @bot.message_handler(func=lambda m: True)
 def country_kitchen(message):
-    if message.text == "кухни мира":
+    if message.text.lower() == "кухни мира":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1 = types.KeyboardButton('Итальянская🇮🇹')
         item2 = types.KeyboardButton('Русская🇷🇺')
@@ -32,7 +44,10 @@ def country_kitchen(message):
         bot.send_message(message.chat.id,
                          "Если не нашли нужную вам страну, то напишите: Кухня %Название страны%".format(
                              message.from_user), reply_markup=markup)
+    # elif message.text.lower() == "":
+
     else:
         bot.reply_to(message, "Извините, но я не понимаю о чем вы")
+
 
 bot.infinity_polling()
